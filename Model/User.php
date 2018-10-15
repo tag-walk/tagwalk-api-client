3e795180548d08e4a2f211a61a3a0374931e0f01
florian <florian@tag-walk.com>
2018-10-15 17:49:57 +0200

    add user model, provider and normalizer

---
+++ Model/User.php
@@ -0,0 +1,461 @@
+<?php declare(strict_types=1);
+/**
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to copyright
+ *
+ * @author      Florian Ajir <florian@tag-walk.com>
+ * @copyright   2016-2018 TAGWALK
+ * @license     proprietary
+ */
+
+namespace Tagwalk\ApiClientBundle\Model;
+
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Tagwalk\ApiClientBundle\Model\Traits\Nameable;
+use Tagwalk\ApiClientBundle\Model\Traits\Sluggable;
+use Tagwalk\ApiClientBundle\Model\Traits\Statusable;
+use Tagwalk\ApiClientBundle\Model\Traits\Timestampable;
+
+/**
+ * Describe User document
+ *
+ * Used for persistance and authentication
+ *
+ * @see Document
+ * @see UserInterface
+ */
+class User implements UserInterface, EquatableInterface
+{
+    use Sluggable;
+    use Statusable;
+    use Timestampable;
+    use Nameable;
+
+    /**
+     * @var string
+     */
+    protected $slug;
+
+    /**
+     * @var string
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     */
+    private $lastname;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string|null
+     */
+    private $gender;
+
+    /**
+     * @var string|null
+     */
+    private $jobTitle;
+
+    /**
+     * @var bool
+     */
+    private $newsletter;
+
+    /**
+     * @var bool|null
+     */
+    private $vip = false;
+
+    /**
+     * @var string|null
+     */
+    private $sector;
+
+    /**
+     * @var string
+     */
+    private $country;
+
+    /**
+     * @var string
+     */
+    private $locale;
+
+    /**
+     * @var string|null
+     */
+    private $salt;
+
+    /**
+     * @var string|null
+     */
+    private $password;
+
+    /**
+     * @var string[]|null
+     */
+    private $roles;
+
+    /**
+     * @var string|null
+     */
+    private $facebookId;
+
+    /**
+     * @param string $name
+     * @param string|null $password
+     * @param string|null $salt
+     * @param array|null $roles
+     */
+    public function __construct($name, ?string $password = null, ?string $salt = null, ?array $roles = null)
+    {
+        $this->name = $name;
+        $this->password = $password;
+        $this->salt = $salt;
+        $this->roles = $roles;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     *
+     * @return self
+     */
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     *
+     * @return self
+     */
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGender(): string
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param string $gender
+     *
+     * @return self
+     */
+    public function setGender(string $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getJobTitle(): ?string
+    {
+        return $this->jobTitle;
+    }
+
+    /**
+     * @param string $jobTitle
+     *
+     * @return self
+     */
+    public function setJobTitle(?string $jobTitle): self
+    {
+        $this->jobTitle = $jobTitle;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNewsletter(): bool
+    {
+        return $this->newsletter;
+    }
+
+    /**
+     * @param bool $newsletter
+     *
+     * @return self
+     */
+    public function setNewsletter(bool $newsletter): self
+    {
+        $this->newsletter = $newsletter;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isVip(): ?bool
+    {
+        return $this->vip;
+    }
+
+    /**
+     * @param bool $vip
+     *
+     * @return self
+     */
+    public function setVip(?bool $vip): self
+    {
+        $this->vip = $vip;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSector(): ?string
+    {
+        return $this->sector;
+    }
+
+    /**
+     * @param string|null $sector
+     *
+     * @return self
+     */
+    public function setSector(?string $sector): self
+    {
+        $this->sector = $sector;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param string $country
+     *
+     * @return self
+     */
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocale(): string
+    {
+        return $this->locale;
+    }
+
+    /**
+     * @param string $locale
+     *
+     * @return self
+     */
+    public function setLocale(string $locale): self
+    {
+        $this->locale = $locale;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSalt(): ?string
+    {
+        return $this->salt;
+    }
+
+    /**
+     * @param string $salt
+     *
+     * @return self
+     */
+    public function setSalt(string $salt): self
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     *
+     * @return self
+     */
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    /**
+     * @param string[] $roles
+     *
+     * @return self
+     */
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Returns the username used to authenticate the user.
+     *
+     * @return string The username
+     */
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        $this->password = null;
+        $this->salt = null;
+        $this->facebookId = null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFacebookId(): ?string
+    {
+        return $this->facebookId;
+    }
+
+    /**
+     * @param string $facebookId
+     *
+     * @return self
+     */
+    public function setFacebookId(?string $facebookId): self
+    {
+        $this->facebookId = $facebookId;
+
+        return $this;
+    }
+
+    /**
+     * The equality comparison should neither be done by referential equality
+     * nor by comparing identities (i.e. getId() === getId()).
+     *
+     * However, you do not need to compare every attribute, but only those that
+     * are relevant for assessing whether re-authentication is required.
+     *
+     * Also implementation should consider that $user instance may implement
+     * the extended user interface `AdvancedUserInterface`.
+     *
+     * @param UserInterface $user
+     *
+     * @return bool
+     */
+    public function isEqualTo(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            return false;
+        }
+        if ($this->email !== $user->getEmail()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     *
+     * @return self
+     */
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullName()
+    {
+        return $this->firstname . ' ' . $this->lastname;
+    }
+}
---
+++ Serializer/Normalizer/UserNormalizer.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types=1);
+/**
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to copyright
+ *
+ * @author      Florian Ajir <florian@tag-walk.com>
+ * @copyright   2016-2018 TAGWALK
+ * @license     proprietary
+ */
+
+namespace Tagwalk\ApiClientBundle\Serializer\Normalizer;
+
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Tagwalk\ApiClientBundle\Model\User;
+
+/**
+ * Normalizer for File instances
+ *
+ * @extends DocumentNormalizer
+ */
+class UserNormalizer extends DocumentNormalizer implements NormalizerInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof User;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsDenormalization($data, $type, $format = null)
+    {
+        return $type === User::class;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function denormalize($data, $class, $format = null, array $context = [])
+    {
+        if (isset($data['created_at'])) {
+            $data['created_at'] = \DateTime::createFromFormat(DATE_ISO8601, $data['created_at']);
+        }
+        if (isset($data['updated_at'])) {
+            $data['updated_at'] = \DateTime::createFromFormat(DATE_ISO8601, $data['updated_at']);
+        }
+
+        return parent::denormalize($data, $class, $format, $context);
+    }
+}
