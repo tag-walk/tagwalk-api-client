38858c528843ce4e0b4c9f2dfccb83070472ebb1
stvalett <stvalett@student.42.fr>
2019-01-21 11:55:46 +0100

    Update model manager

--- Manager/ModelManager.php
+++ Manager/ModelManager.php
@@ -45,4 +45,31 @@ class ModelManager
 
         return $data;
     }
+
+    /**
+     * @param int $from
+     * @param int $size
+     * @param string|null $type
+     * @param string|null $season
+     * @param string|null $city
+     * @param string|null $individuals
+     *
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function list(
+        $from = 0,
+        $size = 10,
+        ?string $type = null,
+        ?string $season = null,
+        ?string $city = null,
+        ?string $individuals = null
+    )
+    {
+        $query = array_filter(compact('from', 'size', 'type', 'season', 'city', 'individuals'));
+        $apiResponse = $this->apiProvider->request('GET', '/api/models', ['query' => $query, 'http_errors' => false]);
+        $data = json_decode($apiResponse->getBody(), true);
+
+        return $data;
+    }
 }
