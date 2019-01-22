a03d50763d7411a2b1b9fb2111c24079994fdbdf
stvalett <stvalett@student.42.fr>
2019-01-22 10:07:29 +0100

    Add Media Manager

--- Manager/MediaManager.php
+++ Manager/MediaManager.php
@@ -4,7 +4,7 @@
  *
  * LICENSE: This source file is subject to copyright
  *
- * @author      Florian Ajir <florian@tag-walk.com>
+ * @author      Steve valette <steve@tag-walk.com>
  * @copyright   2016-2018 TAGWALK
  * @license     proprietary
  */
@@ -13,7 +13,7 @@ namespace Tagwalk\ApiClientBundle\Manager;
 
 use Tagwalk\ApiClientBundle\Provider\ApiProvider;
 
-class ModelManager
+class MediaManager
 {
     /**
      * @var ApiProvider
@@ -29,18 +29,24 @@ class ModelManager
     }
 
     /**
-     * @param string $type
-     * @param string $season
-     * @param string $city
-     * @param int $length
+     * @param int $from
+     * @param int $size
+     * @param string|null $type
+     * @param string|null $season
+     * @param string|null $city
      *
      * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function whoWalkedTheMost($type = null, $season = null, $city = null, $length = 10)
-    {
-        $query = array_filter(compact('type', 'season', 'city', 'length'));
-        $apiResponse = $this->apiProvider->request('GET', '/api/models/who-walked-the-most', ['query' => $query, 'http_errors' => false]);
+    public function list(
+        $from = 0,
+        $size = 10,
+        ?string $type = null,
+        ?string $season = null,
+        ?string $city = null
+    ) {
+        $query = array_filter(compact('from', 'size', 'type', 'season', 'city'));
+        $apiResponse = $this->apiProvider->request('GET', '/api/models', ['query' => $query, 'http_errors' => false]);
         $data = json_decode($apiResponse->getBody(), true);
 
         return $data;
