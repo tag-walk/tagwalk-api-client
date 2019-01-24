5198ff6fa3e23b7979abe85d387d7da6643f45c6
stvalett <stvalett@student.42.fr>
2019-01-24 15:19:24 +0100

    Remove useless function

--- Manager/ModelManager.php
+++ Manager/ModelManager.php
@@ -56,7 +56,7 @@ class ModelManager
      * @return array
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function listModelsMedia(
+    public function listMediasModels(
         $from = 0,
         $size = 10,
         ?string $type = null,
@@ -65,31 +65,6 @@ class ModelManager
     ) {
         $query = array_filter(compact('from', 'size', 'type', 'season', 'city'));
         $apiResponse = $this->apiProvider->request('GET', '/api/models', ['query' => $query, 'http_errors' => false]);
-        $data = json_decode($apiResponse->getBody(), true);
-
-        return $data;
-    }
-
-    /**
-     * @param int $from
-     * @param int $size
-     * @param string|null $type
-     * @param string|null $season
-     * @param string|null $city
-     *
-     * @param string|null $slug
-     * @return array
-     */
-    public function listModelsMediaByName(
-        $from = 0,
-        $size = 10,
-        ?string $type = null,
-        ?string $season = null,
-        ?string $city = null,
-        ?string $slug = null
-    ) {
-        $query = array_filter(compact('from', 'size', 'type', 'season', 'city'));
-        $apiResponse = $this->apiProvider->request('GET', '/api/models/' . $slug, ['query' => $query, 'http_errors' => false]);
         $data = json_decode($apiResponse->getBody(), true);
 
         return $data;
