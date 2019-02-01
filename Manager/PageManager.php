7f62a3c2dbdaba8bf2d59841998e91c8805c74f5
Florian Ajir <florian@tag-walk.com>
2019-02-01 18:11:44 +0100

    add pages manager w/ crud support

---
+++ Manager/PageManager.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to copyright
+ *
+ * @author      Florian Ajir <florian@tag-walk.com>
+ * @copyright   2016-2018 TAGWALK
+ * @license     proprietary
+ */
+
+namespace Tagwalk\ApiClientBundle\Manager;
+
+use Tagwalk\ApiClientBundle\Provider\ApiProvider;
+
+class PageManager
+{
+    const DEFAULT_STATUS = 'enabled';
+    const DEFAULT_SORT = 'created_at:desc';
+
+    /**
+     * @var ApiProvider
+     */
+    private $apiProvider;
+
+    /**
+     * @param ApiProvider $apiProvider
+     */
+    public function __construct(ApiProvider $apiProvider)
+    {
+        $this->apiProvider = $apiProvider;
+    }
+
+    /**
+     * @param int $from
+     * @param int $size
+     * @param string $sort
+     * @param string $status
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function list(
+        $from = 0,
+        $size = 10,
+        $sort = self::DEFAULT_SORT,
+        $status = self::DEFAULT_STATUS
+    ) {
+        $query = array_filter(compact('from', 'size', 'sort', 'status'));
+        $apiResponse = $this->apiProvider->request('GET', '/api/page', ['query' => $query]);
+        $data = json_decode($apiResponse->getBody(), true);
+
+        return $data;
+    }
+
+    /**
+     * @param string $slug
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get(string $slug): array
+    {
+        $apiResponse = $this->apiProvider->request('GET', '/api/page/' . $slug);
+        $data = json_decode($apiResponse->getBody(), true);
+
+        return $data;
+    }
+
+    /**
+     * @param string $slug
+     * @return bool
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function delete(string $slug): bool
+    {
+        $apiResponse = $this->apiProvider->request('DELETE', '/api/page/' . $slug);
+        $data = json_decode($apiResponse->getBody(), true);
+
+        return $data;
+    }
+
+    /**
+     * @param array $record
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function create(array $record): array
+    {
+        $query = ['body' => $record];
+        $apiResponse = $this->apiProvider->request('POST', '/api/page', ['query' => $query]);
+        $data = json_decode($apiResponse->getBody(), true);
+
+        return $data;
+    }
+
+    /**
+     * @param string $slug
+     * @param array $record
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function update(string $slug, array $record): array
+    {
+        $query = ['body' => $record];
+        $apiResponse = $this->apiProvider->request('PUT', '/api/pages/' . $slug, ['query' => $query]);
+        $data = json_decode($apiResponse->getBody(), true);
+
+        return $data;
+    }
+}
