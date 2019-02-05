5d3ad1b33ecdbefc4c4c3b4c75c420dc27dcf46d
Florian Ajir <florian@tag-walk.com>
2019-02-05 16:11:38 +0100

    fix throws ReflectionException (use try-catch in function)

--- Utils/Constants/Constants.php
+++ Utils/Constants/Constants.php
@@ -23,38 +23,44 @@ abstract class Constants
      * Return the list of all declared constants
      *
      * @return array
-     * @throws \ReflectionException
      */
     public static function getAllowedValues()
     {
-        $oClass = new \ReflectionClass(get_called_class());
-        $constants = $oClass->getConstants();
-        foreach ($constants as $key => $constant) {
-            if (false === is_string($constant)) {
-                unset($constants[$key]);
+        try {
+            $oClass = new \ReflectionClass(get_called_class());
+            $constants = $oClass->getConstants();
+            foreach ($constants as $key => $constant) {
+                if (false === is_string($constant)) {
+                    unset($constants[$key]);
+                }
             }
-        }
 
-        return $constants;
+            return $constants;
+        } catch (\ReflectionException $reflectionException) {
+            return [];
+        }
     }
 
     /**
      * Return an associated array of all constants (keys and values are identicals)
      *
      * @return array
-     * @throws \ReflectionException
      */
     public static function getOptions()
     {
-        $oClass = new \ReflectionClass(get_called_class());
-        $constants = $oClass->getConstants();
-        foreach ($constants as $key => $constant) {
-            if (false === is_string($constant)) {
-                unset($constants[$key]);
+        try {
+            $oClass = new \ReflectionClass(get_called_class());
+            $constants = $oClass->getConstants();
+            foreach ($constants as $key => $constant) {
+                if (false === is_string($constant)) {
+                    unset($constants[$key]);
+                }
             }
-        }
-        $constants = array_combine(array_values($constants), array_values($constants));
+            $constants = array_combine(array_values($constants), array_values($constants));
 
-        return $constants;
+            return $constants;
+        } catch (\ReflectionException $reflectionException) {
+            return [];
+        }
     }
 }
