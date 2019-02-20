5312f5e34ab2c2195be78ea1040cf544672e6e7d
Florian Ajir <3715903+florianajir@users.noreply.github.com>
2019-02-20 11:26:02 +0100

    add user manager + set country nullable (#23)
    * add user manager + set country nullable
    
    * refacto

--- Manager/UserManager.php
+++ Manager/UserManager.php
@@ -4,6 +4,7 @@
  *
  * LICENSE: This source file is subject to copyright
  *
+ * @author      Florian Ajir <florian@tag-walk.com>
  * @author      Steve Valette <steve@tag-walk.com>
  * @copyright   2016-2018 TAGWALK
  * @license     proprietary
@@ -11,10 +12,12 @@
 
 namespace Tagwalk\ApiClientBundle\Manager;
 
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 use Tagwalk\ApiClientBundle\Model\User;
 use Tagwalk\ApiClientBundle\Provider\ApiProvider;
-use Symfony\Component\HttpFoundation\Response;
 
 class UserManager
 {
@@ -24,18 +27,39 @@ class UserManager
     private $apiProvider;
 
     /**
-     * @var Serializer
+     * @var SerializerInterface|Serializer
      */
     private $serializer;
 
     /**
      * @param ApiProvider $apiProvider
-     * @param Serializer $serializer
+     * @param SerializerInterface $serializer
      */
-    public function __construct(ApiProvider $apiProvider, Serializer $serializer)
+    public function __construct(ApiProvider $apiProvider, SerializerInterface $serializer)
     {
         $this->apiProvider = $apiProvider;
         $this->serializer = $serializer;
+    }
+
+    /**
+     * @param string $email
+     * @return User|null
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function get(string $email)
+    {
+        $user = null;
+        $apiResponse = $this->apiProvider->request('GET', '/api/users/' . $email, ['http_errors' => false]);
+        if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
+            /** @var User $user */
+            $user = $this->serializer->deserialize(
+                $apiResponse->getBody()->getContents(),
+                User::class,
+                JsonEncoder::FORMAT
+            );
+        }
+
+        return $user;
     }
 
     /**
@@ -65,7 +89,9 @@ class UserManager
     public function update(string $email, User $user)
     {
         $data = $this->serializer->normalize($user, null, ['account' => true]);
-        $data = array_filter($data, function($v) { return $v !== null ; });
+        $data = array_filter($data, function ($v) {
+            return $v !== null;
+        });
         $apiResponse = $this->apiProvider->request('PATCH', '/api/users', [
             'query' => ['email' => $email],
             'json' => $data,
