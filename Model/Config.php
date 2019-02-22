f8e654503872467fdf6db6a1a74d0935838e8389
unikoke94 <barriac.thomas@laposte.net>
2019-02-22 12:52:25 +0100

    [FEAT] Models | add config (#27)
    * add ConfigManager
    
    * add type + transformer
    
    * remove stuff related to bo

---
+++ Model/Config.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to copyright
+ *
+ * @author    Thomas Barriac <thomas@tag-walk.com>
+ * @copyright 2019 TAGWALK
+ * @license   proprietary
+ */
+
+namespace Tagwalk\ApiClientBundle\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Config
+{
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    private $namespace;
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    private $key;
+
+    /**
+     * @var string
+     * @Assert\NotBlank()
+     */
+    private $value;
+
+    /**
+     * @return string
+     */
+    public function getNamespace(): string
+    {
+        return $this->namespace;
+    }
+
+    /**
+     * @param string $namespace
+     * @return self
+     */
+    public function setNamespace(string $namespace): self
+    {
+        $this->namespace = $namespace;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return $this->key;
+    }
+
+    /**
+     * @param string $key
+     * @return self
+     */
+    public function setKey(string $key): self
+    {
+        $this->key = $key;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     * @return self
+     */
+    public function setValue(string $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+}
