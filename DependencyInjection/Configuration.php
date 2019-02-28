a933d4643874f8a7153e240d0546ec09a7b6a87c
Florian Ajir <florian@tag-walk.com>
2019-02-28 15:30:30 +0100

    fix symfony deprecation "A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0."

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,8 +23,13 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('tagwalk_api_client');
+        $treeBuilder = new TreeBuilder('tagwalk_api_client');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('tagwalk_api_client');
+        }
         $rootNode
             ->children()
                 ->arrayNode('api')
