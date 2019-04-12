ad5c6d7285314a755b55cb2b1d7b06c5ee3dcb58
Florian Ajir <florian@tag-walk.com>
2019-04-12 14:45:42 +0200

    implement cache in media manager

--- Manager/MediaManager.php
+++ Manager/MediaManager.php
@@ -13,8 +13,8 @@ namespace Tagwalk\ApiClientBundle\Manager;
 
 use GuzzleHttp\RequestOptions;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Tagwalk\ApiClientBundle\Model\Media;
 use Tagwalk\ApiClientBundle\Provider\ApiProvider;
 use Tagwalk\ApiClientBundle\Serializer\Normalizer\MediaNormalizer;
@@ -37,13 +37,21 @@ class MediaManager
     private $logger;
 
     /**
+     * @var FilesystemAdapter
+     */
+    private $cache;
+
+    /**
      * @param ApiProvider $apiProvider
      * @param MediaNormalizer $mediaNormalizer
+     * @param string $cacheDirectory
+     * @param int $cacheTTL
      */
-    public function __construct(ApiProvider $apiProvider, MediaNormalizer $mediaNormalizer)
+    public function __construct(ApiProvider $apiProvider, MediaNormalizer $mediaNormalizer, int $cacheTTL = 600, string $cacheDirectory = null)
     {
         $this->apiProvider = $apiProvider;
         $this->mediaNormalizer = $mediaNormalizer;
+        $this->cache = new FilesystemAdapter('medias', $cacheTTL, $cacheDirectory);
     }
 
     /**
@@ -60,16 +68,19 @@ class MediaManager
      */
     public function get(string $slug): ?Media
     {
-        $media = null;
-        $apiResponse = $this->apiProvider->request('GET', '/api/medias/' . $slug, [RequestOptions::HTTP_ERRORS => false]);
-        if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
-            $data = json_decode($apiResponse->getBody(), true);
-            $media = $this->mediaNormalizer->denormalize($data, Media::class);
-        } elseif ($apiResponse->getStatusCode() === Response::HTTP_NOT_FOUND) {
-            throw new NotFoundHttpException();
-        } else {
-            $this->logger->error($apiResponse->getBody()->getContents());
-        }
+        $media = $this->cache->get($slug, function () use ($slug) {
+            $data = null;
+            $apiResponse = $this->apiProvider->request('GET', '/api/medias/' . $slug, [RequestOptions::HTTP_ERRORS => false]);
+            if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
+                $data = json_decode($apiResponse->getBody(), true);
+                $data = $this->mediaNormalizer->denormalize($data, Media::class);
+            } elseif ($apiResponse->getStatusCode() !== Response::HTTP_NOT_FOUND) {
+                $this->logger->error($apiResponse->getBody()->getContents());
+            }
+
+            return $data;
+        });
+
 
         return $media;
     }
@@ -85,21 +96,55 @@ class MediaManager
     {
         $media = null;
         if ($type && $season && $designer && $look) {
-            $apiResponse = $this->apiProvider->request(
-                'GET',
-                sprintf('/api/medias/%s/%s/%s/%s', $type, $season, $designer, $look),
-                [RequestOptions::HTTP_ERRORS => false]
-            );
-            if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
-                $data = json_decode($apiResponse->getBody(), true);
-                $media = $this->mediaNormalizer->denormalize($data, Media::class);
-            } elseif ($apiResponse->getStatusCode() === Response::HTTP_NOT_FOUND) {
-                throw new NotFoundHttpException();
-            } else {
-                $this->logger->error($apiResponse->getBody()->getContents());
-            }
+            $media = $this->cache->get(md5(serialize(compact($type, $season, $designer, $look))), function () use ($type, $season, $designer, $look) {
+                $result = null;
+                $apiResponse = $this->apiProvider->request(
+                    'GET',
+                    sprintf('/api/medias/%s/%s/%s/%s', $type, $season, $designer, $look),
+                    [RequestOptions::HTTP_ERRORS => false]
+                );
+                if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
+                    $data = json_decode($apiResponse->getBody(), true);
+                    $result = $this->mediaNormalizer->denormalize($data, Media::class);
+                } elseif ($apiResponse->getStatusCode() !== Response::HTTP_NOT_FOUND) {
+                    $this->logger->error($apiResponse->getBody()->getContents());
+                }
+
+                return $result;
+            });
         }
 
         return $media;
+    }
+
+    /**
+     * @param string $type
+     * @param string $season
+     * @param string $designer
+     * @param string|null $city
+     * @return array|mixed
+     */
+    public function listRelated(string $type, string $season, string $designer, ?string $city = null): array
+    {
+        $query = array_merge([
+            'analytics' => 0,
+            'from' => 0,
+            'size' => 6
+        ], compact($type, $season, $designer, $city));
+        $cacheKey = md5(serialize($query));
+
+        $data = $this->cache->get($cacheKey, function () use ($query) {
+            $results = [];
+            $apiResponse = $this->apiProvider->request('GET', '/api/medias', ['query' => $query, 'http_errors' => false]);
+            if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
+                $results = json_decode($apiResponse->getBody(), true);
+            } else {
+                $this->logger->error($apiResponse->getBody()->getContents());
+            }
+
+            return $results;
+        });
+
+        return $data;
     }
 }
