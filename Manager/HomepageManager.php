21036eb7aa5f4b2e9d54fac1c277352cfc61c743
Florian Ajir <florian@tag-walk.com>
2019-04-18 19:24:23 +0200

    improve homepage manager

--- Manager/HomepageManager.php
+++ Manager/HomepageManager.php
@@ -67,20 +67,59 @@ class HomepageManager
 
     /**
      * @param string $section
+     * @param null|string $language
      * @return Homepage
      */
-    public function getBySection(string $section): ?Homepage
+    public function getBySection(string $section, ?string $language = null): ?Homepage
     {
         if (false === in_array($section, HomepageSection::VALUES)) {
             throw new \InvalidArgumentException('Invalid homepage section argument');
         }
-        $homepage = $this->cache->get($section, function () use ($section) {
+        $key = md5(serialize(compact('section', 'language')));
+        $homepage = $this->cache->get($key, function () use ($section, $language) {
             $record = null;
-            $apiResponse = $this->apiProvider->request(Request::METHOD_GET, "/api/homepages/show/{$section}", [RequestOptions::HTTP_ERRORS => false]);
+            $apiResponse = $this->apiProvider->request(
+                Request::METHOD_GET,
+                "/api/homepages/show/{$section}",
+                [
+                    'query' => ['language' => $language],
+                    RequestOptions::HTTP_ERRORS => false
+                ]
+            );
             if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
                 $record = $this->serializer->deserialize($apiResponse->getBody()->getContents(), Homepage::class, 'json');
             } else {
-                $this->logger->error('$apiResponse->getBody()->getContents()');
+                $this->logger->error($apiResponse->getBody()->getContents());
+            }
+
+            return $record;
+        });
+
+        return $homepage;
+    }
+
+    /**
+     * @param string $slug
+     * @param null|string $language
+     * @return Homepage
+     */
+    public function get(string $slug, ?string $language = null): ?Homepage
+    {
+        $key = md5(serialize(compact('slug', 'language')));
+        $homepage = $this->cache->get($key, function () use ($slug, $language) {
+            $record = null;
+            $apiResponse = $this->apiProvider->request(
+                Request::METHOD_GET,
+                "/api/homepages/{$slug}",
+                [
+                    'query' => ['language' => $language],
+                    RequestOptions::HTTP_ERRORS => false
+                ]
+            );
+            if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
+                $record = $this->serializer->deserialize($apiResponse->getBody()->getContents(), Homepage::class, 'json');
+            } elseif ($apiResponse->getStatusCode() !== Response::HTTP_NOT_FOUND) {
+                $this->logger->error($apiResponse->getBody()->getContents());
             }
 
             return $record;
