bf1565820109e8283eb8ef1d86a1cf63e7daf877
Florian Ajir <florian@tag-walk.com>
2019-04-18 12:27:39 +0200

    Declare HomepageManager service + include cache strategy + add logger

--- Manager/HomepageManager.php
+++ Manager/HomepageManager.php
@@ -11,12 +11,17 @@
 
 namespace Tagwalk\ApiClientBundle\Manager;
 
+use GuzzleHttp\RequestOptions;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Serializer\SerializerInterface;
 use Tagwalk\ApiClientBundle\Model\Cover;
 use Tagwalk\ApiClientBundle\Model\Homepage;
 use Tagwalk\ApiClientBundle\Provider\ApiProvider;
+use Tagwalk\ApiClientBundle\Utils\Constants\HomepageSection;
 
 class HomepageManager
 {
@@ -31,13 +36,35 @@ class HomepageManager
     private $serializer;
 
     /**
+     * @var FilesystemAdapter
+     */
+    private $cache;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
      * @param ApiProvider $apiProvider
      * @param SerializerInterface $serializer
+     * @param int $cacheTTL
+     * @param string $cacheDirectory
      */
-    public function __construct(ApiProvider $apiProvider, SerializerInterface $serializer)
+    public function __construct(ApiProvider $apiProvider, SerializerInterface $serializer, int $cacheTTL = 600, string $cacheDirectory = null)
     {
         $this->apiProvider = $apiProvider;
         $this->serializer = $serializer;
+        $this->cache = new FilesystemAdapter('homepages', $cacheTTL, $cacheDirectory);
+
+    }
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
     }
 
     /**
@@ -47,11 +74,20 @@ class HomepageManager
      */
     public function getBySection(string $section)
     {
-        if (false === in_array($section, Homepage::SECTIONS)) {
+        if (false === in_array($section, HomepageSection::VALUES)) {
             throw new \InvalidArgumentException('Invalid homepage section argument');
         }
-        $apiResponse = $this->apiProvider->request(Request::METHOD_GET, "/api/homepages/show/{$section}");
-        $homepage = $this->serializer->deserialize($apiResponse->getBody()->getContents(), Homepage::class, 'json');
+        $homepage = $this->cache->get($section, function () use ($section) {
+            $record = null;
+            $apiResponse = $this->apiProvider->request(Request::METHOD_GET, "/api/homepages/show/{$section}", [RequestOptions::HTTP_ERRORS => false]);
+            if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
+                $record = $this->serializer->deserialize($apiResponse->getBody()->getContents(), Homepage::class, 'json');
+            } else {
+                $this->logger->error('$apiResponse->getBody()->getContents()');
+            }
+
+            return $record;
+        });
 
         return $homepage;
     }
