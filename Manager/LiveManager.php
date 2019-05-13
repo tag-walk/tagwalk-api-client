d024e0536c67d0bbd286d1e28c659a34590d8fff
Florian Ajir <3715903+florianajir@users.noreply.github.com>
2019-05-13 17:18:20 +0200

    Up/live list light (#73)
    * add light param to live list request
    
    * ensure php-json is installed on system

--- Manager/LiveManager.php
+++ Manager/LiveManager.php
@@ -131,6 +131,7 @@ class LiveManager
             $lives = $cacheItem->get();
             $this->analytics->page('live_list', $query);
         } else {
+            $query['light'] = true;
             $apiResponse = $this->apiProvider->request('GET', '/api/live', [
                 RequestOptions::QUERY => $query,
                 RequestOptions::HTTP_ERRORS => false
