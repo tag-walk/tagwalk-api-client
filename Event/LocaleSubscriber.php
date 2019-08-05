d514c8fa4c9c115c49a3e3de99482897b62a5b8c
Florian Ajir <florian@tag-walk.com>
2019-08-05 17:43:43 +0200

    fix locale subscriber causing high latencies

--- Event/LocaleSubscriber.php
+++ Event/LocaleSubscriber.php
@@ -1,4 +1,5 @@
-<?php
+<?php /** @noinspection NullPointerExceptionInspection */
+
 /**
  * PHP version 7.
  *
@@ -14,7 +15,6 @@ namespace Tagwalk\ApiClientBundle\Event;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
-use Tagwalk\ApiClientBundle\Utils\Constants\Language;
 
 /**
  * Handle kernel request to determine the locale from headers.
@@ -50,23 +50,16 @@ class LocaleSubscriber implements EventSubscriberInterface
     public function onKernelRequest(GetResponseEvent $event): void
     {
         $request = $event->getRequest();
-        $requestLocale = $request->attributes->get('_locale');
-        $session = $request->getSession();
-        if ($requestLocale) {
-            if ($session) {
-                $session->set('_locale', $requestLocale);
-            }
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
         } else {
-            // if no explicit locale has been set on this request:
-            // 1: use one from the session
-            // 2: use prefered from browser config
-            // 3: use application default locale
-            $sessionLocale = $session ? $session->get('_locale') : null;
-            $request->setLocale(
-                $sessionLocale
-                ?? $request->getPreferredLanguage(array_values(Language::getAllowedValues()))
-                ?? $this->defaultLocale
-            );
+            // if no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
         }
     }
 }
