62b45f70aac3c1cb367874fc8a22769fdc6c60b0
stvalett <stvalett@student.42.fr>
2019-09-12 12:17:03 +0200

    Fix style ci showroomUserManager

--- Manager/ShowroomUserManager.php
+++ Manager/ShowroomUserManager.php
@@ -27,95 +27,95 @@ use Tagwalk\ApiClientBundle\Provider\ApiProvider;
 
 class ShowroomUserManager
 {
-	/**
-	 * @var ApiProvider
-	 */
-	private $apiProvider;
-	
-	/**
-	 * @var SerializerInterface|Serializer
-	 */
-	private $serializer;
-	
-	/**
-	 * @var LoggerInterface
-	 */
-	private $logger;
-	
-	/**
-	 * @param ApiProvider         $apiProvider
-	 * @param SerializerInterface $serializer
-	 */
-	public function __construct(ApiProvider $apiProvider, SerializerInterface $serializer)
-	{
-		$this->apiProvider = $apiProvider;
-		$this->serializer = $serializer;
-		$this->logger = new NullLogger();
+    /**
+     * @var ApiProvider
+     */
+    private $apiProvider;
+    
+    /**
+     * @var SerializerInterface|Serializer
+     */
+    private $serializer;
+    
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+    
+    /**
+     * @param ApiProvider         $apiProvider
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(ApiProvider $apiProvider, SerializerInterface $serializer)
+    {
+        $this->apiProvider = $apiProvider;
+        $this->serializer = $serializer;
+        $this->logger = new NullLogger();
 	}
 	
 	/**
-	 * @param LoggerInterface $logger
-	 */
+     * @param LoggerInterface $logger
+     */
 	public function setLogger(LoggerInterface $logger): void
-	{
-		$this->logger = $logger;
-	}
-	
-	/**
-	 * @param ResponseInterface $response
-	 *
-	 * @return ShowroomUser
-	 */
-	private function deserialize($response): ShowroomUser
-	{
-		return $this->serializer->deserialize(
-			$response->getBody()->getContents(),
-			ShowroomUser::class,
-			JsonEncoder::FORMAT
-		);
-	}
-	
-	/**
-	 * @param ShowroomUser $user
-	 *
-	 * @return ShowroomUser|null
-	 */
-	public function create(ShowroomUser $user): ?ShowroomUser
-	{
-		$data = $this->serializer->normalize($user);
-		$data = array_filter($data, static function ($v) {
-				return $v !== null;
-		});
-		$apiResponse = $this->apiProvider->request('POST', '/api/showroom/users/register', [
-				RequestOptions::HTTP_ERRORS => false,
-				RequestOptions::JSON        => $data,
-				RequestOptions::HEADERS     => [
-						'Tagwalk-Showroom-Name' => 'stalker',
-				],
-			]);
-		$created = null;
-		switch ($apiResponse->getStatusCode()) {
-			case Response::HTTP_FORBIDDEN:
-				throw new ApiAccessDeniedException();
-			case Response::HTTP_CREATED:
-				$created = $this->deserialize($apiResponse);
-				break;
-			case Response::HTTP_CONFLICT:
-				$this->logger->notice('ShowroomUser already exists',
-					[
-						'code'    => $apiResponse->getStatusCode(),
-						'message' => $apiResponse->getBody()->getContents(),
-					]);
-				break;
-			default:
-				$this->logger->error('ShowroomUserManager::create unexpected status code',
-					[
-						'code'    => $apiResponse->getStatusCode(),
-						'message' => $apiResponse->getBody()->getContents(),
-					]
-				);
-		}
-		
-		return $created;
-	}
+    {
+        $this->logger = $logger;
+    }
+    
+    /**
+     * @param ResponseInterface $response
+     *
+     * @return ShowroomUser
+     */
+    private function deserialize($response): ShowroomUser
+    {
+        return $this->serializer->deserialize(
+            $response->getBody()->getContents(),
+            ShowroomUser::class,
+            JsonEncoder::FORMAT
+        );
+    }
+    
+    /**
+     * @param ShowroomUser $user
+     *
+     * @return ShowroomUser|null
+     */
+    public function create(ShowroomUser $user): ?ShowroomUser
+    {
+        $data = $this->serializer->normalize($user);
+        $data = array_filter($data, static function ($v) {
+            return $v !== null;
+        });
+        $apiResponse = $this->apiProvider->request('POST', '/api/showroom/users/register', [
+                RequestOptions::HTTP_ERRORS => false,
+                RequestOptions::JSON        => $data,
+                RequestOptions::HEADERS     => [
+                        'Tagwalk-Showroom-Name' => 'stalker',
+                ],
+            ]);
+        $created = null;
+        switch ($apiResponse->getStatusCode()) {
+            case Response::HTTP_FORBIDDEN:
+                throw new ApiAccessDeniedException();
+            case Response::HTTP_CREATED:
+                $created = $this->deserialize($apiResponse);
+                break;
+            case Response::HTTP_CONFLICT:
+                $this->logger->notice('ShowroomUser already exists',
+                    [
+                        'code'    => $apiResponse->getStatusCode(),
+                        'message' => $apiResponse->getBody()->getContents(),
+                    ]);
+                break;
+            default:
+                $this->logger->error('ShowroomUserManager::create unexpected status code',
+                    [
+                        'code'    => $apiResponse->getStatusCode(),
+                        'message' => $apiResponse->getBody()->getContents(),
+                    ]
+                );
+        }
+        
+        return $created;
+    }
 }
