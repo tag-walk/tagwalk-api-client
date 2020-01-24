a7efe6c8add55aa172cdad36964edd663e4bb30d
stvalett <stvalett@student.42.fr>
2020-01-24 10:37:23 +0100

    Fix phpDoc

--- Manager/FilterManager.php
+++ Manager/FilterManager.php
@@ -44,8 +44,8 @@ class FilterManager
      * $params['season']        = (string) The season selected to restrict the results (Required false)
      * $params['designers']     = (string) The designers seleted to restrict the results (Required false)
      * $params['individual']    = (string) The individual selected to restrict the results (Required false)
-     * $params['tags']          = (string)   A comma-seperated string of tags to restrict the results (Required false)
-     * $params['language']      = (string) locale language.
+     * $params['tags']          = (string) A comma-seperated string of tags to restrict the results (Required false)
+     * $params['language']      = (string) Locale language.
      *
      * @param array $params
      *
