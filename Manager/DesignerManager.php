e0b944dd33128217b7ab80aad0700bc4c2cf1f20
stvalett <stvalett@student.42.fr>
2020-02-20 11:14:33 +0100

    Add query "country" to display designer affected to a country (#168)
    * Add query "country" to display designer affected to a country
    
    * Add query country in filter list
    
    * Update annotation

--- Manager/DesignerManager.php
+++ Manager/DesignerManager.php
@@ -97,6 +97,7 @@ class DesignerManager
      * @param bool        $talent
      * @param bool        $tagbook
      * @param bool        $denormalize
+     * @param string|null $country
      *
      * @return array|Designer[]
      */
@@ -108,10 +109,11 @@ class DesignerManager
         string $status = self::DEFAULT_STATUS,
         bool $talent = false,
         bool $tagbook = false,
-        bool $denormalize = true
+        bool $denormalize = true,
+        ?string $country = null
     ): array {
         $designers = [];
-        $query = array_filter(compact('from', 'size', 'sort', 'status', 'language', 'talent', 'tagbook'));
+        $query = array_filter(compact('from', 'size', 'sort', 'status', 'language', 'talent', 'tagbook', 'country'));
         $apiResponse = $this->apiProvider->request('GET', '/api/designers', [
             RequestOptions::HTTP_ERRORS => false,
             RequestOptions::QUERY       => $query,
@@ -194,6 +196,7 @@ class DesignerManager
      * @param null|string $models
      * @param bool|null   $talent
      * @param string|null $language
+     * @param string|null $country
      *
      * @return array
      */
@@ -204,10 +207,11 @@ class DesignerManager
         ?string $tags,
         ?string $models,
         ?bool $talent = false,
-        ?string $language = null
+        ?string $language = null,
+        ?string $country = null
     ): array {
         $results = [];
-        $query = array_filter(compact('type', 'season', 'city', 'tags', 'models', 'talent', 'language'));
+        $query = array_filter(compact('type', 'season', 'city', 'tags', 'models', 'talent', 'language', 'country'));
         $apiResponse = $this->apiProvider->request('GET', '/api/designers/filter', [
             RequestOptions::HTTP_ERRORS => false,
             RequestOptions::QUERY       => $query,
