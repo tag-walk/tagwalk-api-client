36fb4b3e735249f4dcd7407463b9de31ada1a0d5
stvalett <stvalett@student.42.fr>
2020-02-20 11:46:00 +0100

    Update phpDoc

--- Manager/FilterManager.php
+++ Manager/FilterManager.php
@@ -74,9 +74,10 @@ class FilterManager
      * $params['city']          = (string) The city selected to restrict the results (Required false)
      * $params['season']        = (string) The season selected to restrict the results (Required false)
      * $params['designers']     = (string) A comma-seperated string of designers seleted to restrict the results (Required false)
-     * $params['individual']    = (string) The individual selected to restrict the results (Required false)
+     * $params['model']         = (string) The individual selected to restrict the results (Required false)
      * $params['tags']          = (string) A comma-seperated string of tags to restrict the results (Required false)
      * $params['category']      = (string) The accesory_category selected to restrict the results (Required false)
+     * $params['includes']      = (string) The comma-seperated (designer, city, season, models, category, tags) allow multiple values (Require true)
      * $params['language']      = (string) Locale language.
      *
      * @param array $params
