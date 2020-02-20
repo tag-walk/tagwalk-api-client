d2d0553a9d22acd6a2c6b2938cb333a28114e8d8
stvalett <stvalett@student.42.fr>
2020-02-20 10:32:45 +0100

    Add getlookFilter

--- Manager/FilterManager.php
+++ Manager/FilterManager.php
@@ -61,7 +61,39 @@ class FilterManager
         if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
             $data = json_decode($apiResponse->getBody()->getContents(), true);
         } else {
-            $this->logger->error('StreetstyleManager::adaptiveFiltersList unexpected status code', [
+            $this->logger->error('FilterManager::getStreetFilter unexpected status code', [
+                'code'    => $apiResponse->getStatusCode(),
+                'message' => $apiResponse->getBody()->getContents(),
+            ]);
+        }
+
+        return $data;
+    }
+
+    /**
+     * $params['city']          = (string) The city selected to restrict the results (Required false)
+     * $params['season']        = (string) The season selected to restrict the results (Required false)
+     * $params['designers']     = (string) A comma-seperated string of designers seleted to restrict the results (Required false)
+     * $params['individual']    = (string) The individual selected to restrict the results (Required false)
+     * $params['tags']          = (string) A comma-seperated string of tags to restrict the results (Required false)
+     * $params['category']      = (string) The accesory_category selected to restrict the results (Required false)
+     * $params['language']      = (string) Locale language.
+     *
+     * @param array $params
+     *
+     * @return array
+     */
+    public function getLookFilter(array $params): array
+    {
+        $data = [];
+        $apiResponse = $this->apiProvider->request('GET', '/api/medias/adaptive-filters', [
+            RequestOptions::HTTP_ERRORS => false,
+            RequestOptions::QUERY       => $params,
+        ]);
+        if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
+            $data = json_decode($apiResponse->getBody()->getContents(), true);
+        } else {
+            $this->logger->error('FilterManager::getLookFilter unexpected status code', [
                 'code'    => $apiResponse->getStatusCode(),
                 'message' => $apiResponse->getBody()->getContents(),
             ]);
