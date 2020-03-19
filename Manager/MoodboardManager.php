c8f8844657befca05b362ee7eafd95f826a8a2b0
Florian Ajir <3715903+florianajir@users.noreply.github.com>
2020-03-19 15:54:21 +0100

    fix MoodboardManager throws ApiAccessDeniedException (#195)
    * update MoodboardManager::list throws client exception like 403 error to redirect to login
    
    * throw ApiAccessDeniedException on moodboard endpoints 403 response

--- Manager/MoodboardManager.php
+++ Manager/MoodboardManager.php
@@ -78,6 +78,9 @@ class MoodboardManager
             RequestOptions::QUERY       => $params,
             RequestOptions::HTTP_ERRORS => false,
         ]);
+        if ($apiResponse->getStatusCode() === Response::HTTP_FORBIDDEN) {
+            throw new ApiAccessDeniedException();
+        }
         if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
             $data = json_decode($apiResponse->getBody(), true);
             if (!empty($data)) {
@@ -102,6 +105,9 @@ class MoodboardManager
             RequestOptions::QUERY       => $params,
             RequestOptions::HTTP_ERRORS => false,
         ]);
+        if ($apiResponse->getStatusCode() === Response::HTTP_FORBIDDEN) {
+            throw new ApiAccessDeniedException();
+        }
 
         return (int) $apiResponse->getHeaderLine('X-Total-Count');
     }
@@ -263,6 +269,9 @@ class MoodboardManager
     {
         $params = [RequestOptions::JSON => $this->serializer->normalize($moodboard, null, ['write' => true])];
         $apiResponse = $this->apiProvider->request(Request::METHOD_PUT, '/api/moodboards/'.$slug, array_merge($params, [RequestOptions::HTTP_ERRORS => false]));
+        if ($apiResponse->getStatusCode() === Response::HTTP_FORBIDDEN) {
+            throw new ApiAccessDeniedException();
+        }
         if ($apiResponse->getStatusCode() === Response::HTTP_OK) {
             return $this->denormalizeResponse($apiResponse);
         }
