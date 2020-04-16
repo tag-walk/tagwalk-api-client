afaf2dd78918448d3f10e1e2ed1dd14f303408c1
Florian Ajir <florian@tag-walk.com>
2020-04-17 00:53:53 +0200

    add .editorconfig + max_line_length = 120

--- Manager/FilterManager.php
+++ Manager/FilterManager.php
@@ -33,7 +33,8 @@ class FilterManager
     /**
      * $params['city']          = (string) The city selected to restrict the results (Required false)
      * $params['season']        = (string) The season selected to restrict the results (Required false)
-     * $params['designers']     = (string) A comma-seperated string of designers seleted to restrict the results (Required false)
+     * $params['designers']     = (string) A comma-seperated string of designers seleted to restrict the results
+     *                            (Required false)
      * $params['individual']    = (string) The individual selected to restrict the results (Required false)
      * $params['tags']          = (string) A comma-seperated string of tags to restrict the results (Required false)
      * $params['language']      = (string) Locale language.
@@ -59,11 +60,13 @@ class FilterManager
     /**
      * $params['city']          = (string) The city selected to restrict the results (Required false)
      * $params['season']        = (string) The season selected to restrict the results (Required false)
-     * $params['designers']     = (string) A comma-seperated string of designers seleted to restrict the results (Required false)
+     * $params['designers']     = (string) A comma-seperated string of designers seleted to restrict the results
+     *                            (Required false)
      * $params['model']         = (string) The individual selected to restrict the results (Required false)
      * $params['tags']          = (string) A comma-seperated string of tags to restrict the results (Required false)
      * $params['category']      = (string) The accesory_category selected to restrict the results (Required false)
-     * $params['includes']      = (string) The comma-seperated (designer, city, season, models, category, tags) allow multiple values (Require true)
+     * $params['includes']      = (string) The comma-seperated (designer, city, season, models, category, tags)
+     *                            allow multiple values (Require true)
      * $params['language']      = (string) Locale language.
      *
      * @param array $params
