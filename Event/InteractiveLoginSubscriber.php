a3269237dafd31c3713609f9289c8faa67fd2ed0
Florian Ajir <3715903+florianajir@users.noreply.github.com>
2020-04-29 17:59:04 +0200

    Fix/user abstract (#219)
    * fix user class abstraction in api client auth process to be able to login with in memory users
    
    * throw ApiServerErrorException if 400 on designer list

--- Event/InteractiveLoginSubscriber.php
+++ Event/InteractiveLoginSubscriber.php
@@ -15,6 +15,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Http\SecurityEvents;
+use Tagwalk\ApiClientBundle\Model\User;
 
 /**
  * Stores the locale of the user in the session after the
@@ -57,10 +58,10 @@ class InteractiveLoginSubscriber implements EventSubscriberInterface
      *
      * @param InteractiveLoginEvent $event
      */
-    public function setUserLocale(InteractiveLoginEvent $event): void
+    final public function setUserLocale(InteractiveLoginEvent $event): void
     {
         $user = $event->getAuthenticationToken()->getUser();
-        if (null !== $user->getLocale()) {
+        if ($user instanceof User && null !== $user->getLocale()) {
             $this->session->set('_locale', $user->getLocale());
         }
     }
