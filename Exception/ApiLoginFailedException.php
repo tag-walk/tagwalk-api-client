07dec4855f861e0fb2f7bd87e4133b260a2c91a4
Thomas Barriac <thomas@tag-walk.com>
2020-06-20 19:38:19 +0200

    add new exception

---
+++ Exception/ApiLoginFailedException.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to copyright
+ *
+ * @author    Thomas Barriac <thomas@tag-walk.com>
+ * @copyright 2020 TAGWALK
+ * @license   proprietary
+ */
+
+namespace Tagwalk\ApiClientBundle\Exception;
+
+use \Exception;
+
+class ApiLoginFailedException extends Exception
+{
+    public function __construct($message = 'Api Login Failed', int $code = 400)
+    {
+        parent::__construct($message, $code);
+    }
+}
