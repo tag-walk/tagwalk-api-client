15149a5bee009d5c934906632f15e98cab22ccf0
Matthew MOSTRAG <matthew@tag-walk.com>
2020-08-26 15:21:09 +0200

    Create model and manager for newsletter insights

---
+++ Manager/NewsletterInsightManager.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tagwalk\ApiClientBundle\Manager;
+
+use GuzzleHttp\RequestOptions;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
+use Tagwalk\ApiClientBundle\Model\NewsletterInsight;
+use Tagwalk\ApiClientBundle\Provider\ApiProvider;
+use Tagwalk\ApiClientBundle\Utils\Constants\Status;
+
+class NewsletterInsightManager
+{
+    private const DEFAULT_SIZE = 10;
+    private const DEFAULT_SORT = 'sent_at:desc';
+
+    private ApiProvider $apiProvider;
+    private SerializerInterface $serializer;
+
+    public function __construct(ApiProvider $apiProvider, SerializerInterface $serializer)
+    {
+        $this->apiProvider = $apiProvider;
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @return NewsletterInsight[]
+     */
+    public function list(
+        string $sort = self::DEFAULT_SORT,
+        int $from = 0,
+        int $size = self::DEFAULT_SIZE,
+        string $status = Status::ENABLED
+    ): array {
+        $query = array_filter(compact('sort', 'from', 'size', 'status'));
+        $apiResponse = $this->apiProvider->request('GET', '/api/newsletter/insights', [
+            RequestOptions::QUERY => $query,
+            RequestOptions::HTTP_ERRORS => false,
+        ]);
+
+        $data = [];
+        if ($apiResponse->getStatusCode() !== Response::HTTP_OK) {
+            return $data;
+        }
+
+        $data = json_decode($apiResponse->getBody(), true);
+        foreach ($data as $i => $datum) {
+            $data[$i] = $this->serializer->denormalize($datum, NewsletterInsight::class);
+        }
+
+        return $data;
+    }
+}
