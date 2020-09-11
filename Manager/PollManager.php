c0572373d7af506b05ee187d73610057515d2243
Matthew MOSTRAG <matthew@tag-walk.com>
2020-09-11 15:24:02 +0200

    Add get to poll manager

--- Manager/PollManager.php
+++ Manager/PollManager.php
@@ -4,6 +4,7 @@ namespace Tagwalk\ApiClientBundle\Manager;
 
 use GuzzleHttp\RequestOptions;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\SerializerInterface;
 use Tagwalk\ApiClientBundle\Model\Poll;
 use Tagwalk\ApiClientBundle\Model\PollAnswer;
@@ -31,9 +32,26 @@ class PollManager
             return null;
         }
 
-        $data = json_decode($apiResponse->getBody(), true);
+        $data = json_decode($apiResponse->getBody());
         /** @var Poll $data */
         $data = $this->serializer->denormalize($data, Poll::class);
+
+        return $data;
+    }
+
+    public function get(int $id, ?string $language = null): ?Poll
+    {
+        $apiResponse = $this->apiProvider->request('GET', '/api/poll/' . $id, [
+            RequestOptions::HTTP_ERRORS => false,
+            RequestOptions::QUERY => array_filter(['language' => $language]),
+        ]);
+
+        if ($apiResponse->getStatusCode() !== Response::HTTP_OK) {
+            return null;
+        }
+
+        /** @var Poll $data */
+        $data = $this->serializer->deserialize($apiResponse->getBody(), Poll::class, JsonEncoder::FORMAT);
 
         return $data;
     }
