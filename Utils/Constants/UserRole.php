965acf657171da1909c04ea52f9b0dfcf72558a5
Thomas Barriac <thomas@tag-walk.com>
2021-02-18 14:54:19 +0100

    add use of new endpoint + constant roles

---
+++ Utils/Constants/UserRole.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * PHP version 7
+ *
+ * LICENSE: This source file is subject to copyright
+ *
+ * @author    Thomas Barriac <thomas@tag-walk.com>
+ * @copyright 2021 TAGWALK
+ * @license   proprietary
+ */
+
+namespace Tagwalk\ApiClientBundle\Utils\Constants;
+
+class UserRole extends Constants
+{
+    const USER = 'ROLE_USER';
+    const EMPLOYEE = 'ROLE_EMPLOYEE';
+    const ADMIN = 'ROLE_ADMIN';
+}
