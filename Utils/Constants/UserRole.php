3f0cbe51216e73358f7f6f3f10f1c35521775d05
Kévin Masseix <contact@mkcg.io>
2021-04-14 20:15:57 +0200

    [User roles] declare the roles used by the showroom app

--- Utils/Constants/UserRole.php
+++ Utils/Constants/UserRole.php
@@ -16,4 +16,7 @@ class UserRole extends Constants
     const USER = 'ROLE_USER';
     const EMPLOYEE = 'ROLE_EMPLOYEE';
     const ADMIN = 'ROLE_ADMIN';
+    const BRAND_MANAGER = 'ROLE_BRAND_MANAGER';
+    const USER_MANAGER = 'ROLE_USER_MANAGER';
+    const SUPER_MANAGER = 'ROLE_SUPER_MANAGER';
 }
